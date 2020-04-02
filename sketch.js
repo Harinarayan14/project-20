062386cd4467cc23cbcfc4e2c553013318d69f1d
Harinarayan14 <62546869+Harinarayan14@users.noreply.github.com>
2020-04-02 07:39:55 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,10 +9,11 @@ function draw(){
   
    fill("black");
   stroke('red');
-  ellipse(mouseX,mouseY,50,50);
- 
+  
   let r = map(mouseX,0,1200,0,255,true);
   let g = map(mouseY,0,400,0,255,true);
   let b = map(mouseX,400,1200,0,255,true);
   background(r,g,b);
+  ellipse(mouseX,mouseY,50,50);
+ 
 }
